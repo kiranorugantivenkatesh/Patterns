262ee0ba9a28e6dbdb78e0b1a0fba1b38bb5334a
kiranorugantivenkatesh <kiranoruganti21@gmail.com>
2025-02-13 17:28:27 -0500

    Star pattern.

---
+++ src/pattern1/pattern2.java
@@ -0,0 +1,24 @@
+package pattern1;
+/*
+    Print the following pattern for the given N number of rows
+    Pattern for N = 4;
+    ****
+    ****
+    ****
+    ****
+
+*/
+public class pattern2 {
+    public static void main(String[] args) {
+        printPattern1(4);
+    }
+
+    public static void printPattern1(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
